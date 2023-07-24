74f8c2bbcebb814e28d4b516ee00a6407382a951
Dohyung Kim <kdkim7676@gmail.com>
2023-07-23 19:27:55 -0700

    added email and secret key to header to verify if user is logged in before accessing backend

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,6 +1,40 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
+const validateUserCredentials = (email, password) => {
+    if (email.length === 0) {
+        return {
+            result : false,
+            message : "Missing email",
+            status : 400
+        }
+    }
+
+    if (password.length === 0) {
+        return {
+            result : false,
+            message : "Missing password",
+            status : 400
+        }
+    }
+
+    // Validate email format
+    // Ref: https://www.w3resource.com/javascript/form/email-validation.php
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)===false) {
+        return {
+            result : false,
+            message : "Invalid email format",
+            status : 400
+        }
+    }
+
+    // add more or make it a separate file for this f
+
+    return {
+        result : true
+    }
+}
+
 exports.login = (req, res) => {
     let users = []
     fs.createReadStream('./users.csv')
@@ -10,7 +44,13 @@ exports.login = (req, res) => {
     })
     .on('end', () => {
         const { email, password } = req.body
-        console.log(req)
+        let validator = validateUserCredentials(email, password)
+        if(!validator.result){
+            res.status(validator.status).send({
+                message: validator.message
+            })
+            return
+        }
         const user = users.find((u) => u.email === email && u.password === password);
         const isUserFound = !!user;
 
@@ -18,13 +58,16 @@ exports.login = (req, res) => {
         if (isUserFound) {
             // Successful login, return 200 status code.
             res.status(200).send({
-                message: 'Login successful!'
+                message: 'Login successful!',
+
+                // this can be a JWT token...
+                secret_key: Buffer.from(email).toString('base64')
             });
         } else {
             // Login failed, return 400 status code.
             res.status(400).send({
-                error: 'Invalid email or password'
+                message: 'Invalid email or password'
             });
         }
     });
-} 
+}
--- controller/vehiclesController.js
+++ controller/vehiclesController.js
@@ -2,6 +2,12 @@ const csv = require('csv-parser');
 const fs = require('fs');
 
 exports.getVehicles = (req, res) => {
+    const email = req.header('email')
+    const decoded_key = Buffer.from(req.header('secret_key'), 'base64').toString('utf8')
+    if ( decoded_key !== email ) {
+        return res.status(401).send('Unauthorized');
+    }
+
     let results = []
     fs.createReadStream('./vehicles.csv')
         .pipe(csv())
@@ -12,10 +18,11 @@ exports.getVehicles = (req, res) => {
             const offset = parseInt(req.query.offset) || 0;
             const limit = parseInt(req.query.limit) || results.length;
 
-            results = results.slice(offset, offset + limit);
+            vehicles = results.slice(offset, offset + limit);
 
             res.status(200).send({
-                vehicles: results
+                vehicles: vehicles,
+                total: results.length
             });
         });
 };
